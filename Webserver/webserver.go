12f9fc7d08be2204df430bb894c54770f0983fb1
Sandeep Bhatia <sabhatia@gmail.com>
2020-08-28 11:15:46 -0700

    Fixed variable names

--- Webserver/webserver.go
+++ Webserver/webserver.go
@@ -1,5 +1,3 @@
-// +build ignore
-
 package main
 
 import (
@@ -11,43 +9,43 @@ import (
 	"time"
 )
 
-var rsp_id = 0
+var rspID = 0
 var rnd *rand.Rand
-var DEFAULT_FAIL_RATE = 10    // percent
-var DEFAULT_FAIL_TIMEOUT = 10 // milliseconds
-var DEFAULT_SERVER_PORT = 8080
-var ss server_settings // hate this!
 
-type server_settings struct {
-	failure_rate    int
-	failure_timeout int
-	server_port     string
+const DefaultFailRate = 10    // percent
+const DefaultFailTimeout = 10 // milliseconds
+const DefaultServerPort = "8080"
+
+var ss serverSettings // hate this!
+
+type serverSettings struct {
+	failureRate    int
+	failureTimeout int
+	serverPort     string
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	rsp_id++
-	if rnd.Intn(100) > ss.failure_rate {
+	rspID++
+	if rnd.Intn(100) > ss.failureRate {
 		fmt.Fprintf(w, "Hello! You've requested %s!\n", r.URL.Path)
-		fmt.Fprintf(w, "Respose Id: %d!\n", rsp_id)
+		fmt.Fprintf(w, "Respose Id: %d!\n", rspID)
 	} else {
-		time.Sleep(time.Duration(ss.failure_timeout) * time.Millisecond)
+		time.Sleep(time.Duration(ss.failureTimeout) * time.Millisecond)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("500 - Simulated a server failure!\n"))
-		fmt.Fprintf(w, "Respose Id: %d!\n", rsp_id)
+		fmt.Fprintf(w, "Respose Id: %d!\n", rspID)
 	}
 }
 
-func parseArgs(ss *server_settings) {
-	f_r := flag.Int("failure-rate", 10, "the rate of failures to simulate (in percent)")
-	f_to := flag.Int("failure-timeout", 10, "the timeout in case of failure (in msecs)")
-	s_p := flag.String("server-port", "8080", "the server port to listen for HTTP requests")
+func parseArgs(ss *serverSettings) {
+	fR := flag.Int("failure-rate", DefaultFailRate, "the rate of failures to simulate (in percent)")
+	fTO := flag.Int("failure-timeout", DefaultFailTimeout, "the timeout in case of failure (in msecs)")
+	sP := flag.String("server-port", DefaultServerPort, "the server port to listen for HTTP requests")
 	flag.Parse()
 
-	ss.failure_rate = *f_r
-	ss.failure_timeout = *f_to
-	ss.server_port = ":" + *s_p
-
-	fmt.Printf("%+v\n", ss)
+	ss.failureRate = *fR
+	ss.failureTimeout = *fTO
+	ss.serverPort = ":" + *sP
 }
 
 func main() {
@@ -59,5 +57,5 @@ func main() {
 
 	// start the server
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(ss.server_port, nil))
+	log.Fatal(http.ListenAndServe(ss.serverPort, nil))
 }
